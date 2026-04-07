656a0c05ee9fdf1843df272b0db613740c5fa49c
Nabarupa <nabarupabhunia27@gmail.com>
2026-04-07 15:20:48 +0530

    fix render port timeout

--- app.js
+++ app.js
@@ -115,6 +115,8 @@ app.use((err, req, res, next) => {
 });
 
 // SERVER
-app.listen(8080, () => {
-  console.log("server is listening on port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
 });
