402f7c45a595c533017d48aa3a3001a04e3493fe
Nabarupa <nabarupabhunia27@gmail.com>
2026-04-08 17:31:37 +0530

    final fix listing controller

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,8 +1,7 @@
 const Listing = require("../models/listing");
 const ExpressError = require("../utils/ExpressError");
-const { cloudinary } = require("../utils/cloudinary");
 
-//INDEX
+// INDEX
 module.exports.index = async (req, res) => {
   const { category, search } = req.query;
   let query = {};
@@ -21,54 +20,28 @@ module.exports.index = async (req, res) => {
   res.render("listings/index.ejs", { allListings, category, search });
 };
 
-//SEARCH DROPDOWN API
-module.exports.searchSuggestions = async (req, res) => {
-  const { q } = req.query;
-  if (!q) return res.json([]);
-
-  const results = await Listing.find({
-    $or: [
-      { title: new RegExp(q, "i") },
-      { location: new RegExp(q, "i") },
-      { country: new RegExp(q, "i") },
-    ],
-  })
-    .limit(5)
-    .select("title location country");
-
-  res.json(
-    results.map(r =>
-      r.location ? `${r.location} - ${r.title}` : r.title
-    )
-  );
-};
-
-//NEW FORM
+// NEW FORM
 module.exports.renderNewForm = (req, res) => {
   res.render("listings/new.ejs");
 };
 
-//CREATE
+// CREATE (FINAL FIXED)
 module.exports.createListing = async (req, res) => {
   try {
-    const listing = new Listing(req.body.listing);
+    // SAFE BODY FIX
+    const data = req.body.listing || req.body;
+
+    const listing = new Listing(data);
     listing.owner = req.user._id;
 
-    // if image upload 
-    if (req.file) {
-      listing.image = {
-        url: req.file.path,
-        filename: req.file.filename,
-      };
-    } else {
-      // fallback image (IMPORTANT)
-      listing.image = {
-        url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
-        filename: "default",
-      };
-    }
+    // ALWAYS DEFAULT IMAGE
+    listing.image = {
+      url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+      filename: "default",
+    };
 
     await listing.save();
+
     req.flash("success", "New listing created");
     res.redirect("/listings");
 
@@ -78,11 +51,9 @@ module.exports.createListing = async (req, res) => {
   }
 };
 
-//SHOW
+// SHOW
 module.exports.showListing = async (req, res, next) => {
-  const { id } = req.params;
-
-  const listing = await Listing.findById(id)
+  const listing = await Listing.findById(req.params.id)
     .populate("owner")
     .populate({
       path: "reviews",
@@ -93,53 +64,35 @@ module.exports.showListing = async (req, res, next) => {
   res.render("listings/show.ejs", { listing });
 };
 
-//EDIT FORM
+// EDIT FORM
 module.exports.renderEditForm = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if (!listing) return next(new ExpressError(404, "Listing not found"));
   res.render("listings/edit.ejs", { listing });
 };
 
-//UPDATE
+// UPDATE 
 module.exports.updateListing = async (req, res) => {
+  const data = req.body.listing || req.body;
+
   const listing = await Listing.findByIdAndUpdate(
     req.params.id,
-    { ...req.body.listing },
+    data,
     { new: true }
   );
-
-  // only update image if uploaded
-  if (req.file) {
-    if (listing.image?.filename && listing.image.filename !== "default") {
-      await cloudinary.uploader.destroy(listing.image.filename);
-    }
-
-    listing.image = {
-      url: req.file.path,
-      filename: req.file.filename,
-    };
-
-    await listing.save();
-  }
 
   req.flash("success", "Listing updated");
   res.redirect(`/listings/${listing._id}`);
 };
 
-//DELETE
+// DELETE
 module.exports.deleteListing = async (req, res) => {
-  const listing = await Listing.findById(req.params.id);
-
-  if (listing.image?.filename) {
-    await cloudinary.uploader.destroy(listing.image.filename);
-  }
-
   await Listing.findByIdAndDelete(req.params.id);
   req.flash("success", "Listing deleted");
   res.redirect("/listings");
 };
 
-// wishlist toggle
+// WISHLIST
 module.exports.toggleWishlist = async (req, res) => {
   const { id } = req.params;
   const user = req.user;
@@ -157,9 +110,5 @@ module.exports.toggleWishlist = async (req, res) => {
   }
 
   await user.save();
-
-  // always safe
   res.redirect("/listings");
 };
-
-
