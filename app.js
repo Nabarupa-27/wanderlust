3c2903b3d64fe0cb032ed1abeb5c2055793fa90e
Nabarupa <nabarupabhunia27@gmail.com>
2026-04-09 00:38:37 +0530

    fix render port binding

--- app.js
+++ app.js
@@ -102,6 +102,6 @@ app.use((err, req, res, next) => {
 // SERVER
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`server is listening on port ${PORT}`);
 });
