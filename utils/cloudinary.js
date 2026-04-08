06274c501764f4877028a955667ad0024ea09a57
Nabarupa <nabarupabhunia27@gmail.com>
2026-04-09 01:13:03 +0530

    fix cloudinary format bug

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "Wanderlust",
-    allowedFormats: ["jpeg", "png", "jpg"],
+    allowed_formats: ["jpeg", "png", "jpg"], 
   },
 });
 
